ffdc350649184796138cd719152cc0150d6e2a18
Luis Costantin <luisclaudioc@hotmail.com>
2024-07-14 15:15:54 +0100

    contact form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ContactForm } from './ContactForm'
 
 function Contact({ isActive }) {
 
@@ -9,18 +10,21 @@ function Contact({ isActive }) {
     return (
         <>
             <div className='Contact'>
-                <div>
-                    <h1>my_contact_info:</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <a href='google.com' className="link" target="_blank">LinkedIn</a>, <a href='google.com' className="link" target="_blank">GitHub</a> or <a href='google.com' className="link" target="_blank">Instagram</a></p>
+                <div className='contact-one'>
+                    <div>
+                        <h1>my_contact_info:</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <a href='google.com' className="link" target="_blank">LinkedIn</a>, <a href='google.com' className="link" target="_blank">GitHub</a> or <a href='google.com' className="link" target="_blank">Instagram</a></p>
+                    </div>
+                    <div className='contact-info'>
+                        <p>+351 938 349 977</p>
+                        <p>luisclaudioc@hotmail.com</p>
+                        <p>rua cidade de cardiff 17 r/c</p>
+                        <p>1170-094, lisboa, pt</p>
+                    </div>
                 </div>
-                <div className='contact-info'>
-                    <p>+351 938 349 977</p>
-                    <p>luisclaudioc@hotmail.com</p>
-                    <p>rua cidade de cardiff 17 r/c</p>
-                    <p>1170-094, lisboa, pt</p>
-                </div>
+                <ContactForm />
             </div>
-            
+
             <div style={{opacity: 0}}>.</div>
         </>
     )
---
+++ src/components/Contact/ContactForm.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+export function ContactForm() {
+
+    const [ name, setName ] = useState("");
+    const [ email, setEmail ] = useState("");
+    const [ message, setMessage ] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setName("");
+        setEmail("");
+        setMessage("");
+    }
+    
+    return (
+        <div className='contact-form'>
+            <h1>contact_me_directly:</h1>
+            <form onSubmit={handleSubmit}>
+                <input name='name' type='text' value={name} onChange={(e) => setName(e.target.value)} placeholder='your name' />
+                <input name='email' type='email' value={email} onChange={(e) => setEmail(e.target.value)}  placeholder='your email' />
+                <textarea name='message' value={message} onChange={(e) => setMessage(e.target.value)} placeholder='your message' />
+                <input type='submit' value='Send' className='submit-btn'/>
+            </form>
+        </div>
+    )
+}
