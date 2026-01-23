6cd5efad7c3fa19805feb725107f735426f8644d
Luis Costantin <luisclaudioc@hotmail.com>
2026-01-23 12:39:46 +0000

    Update contact address to new location in Munich

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -13,8 +13,8 @@ function Contact({ isActive }) {
                 <h1>my_contact_info:</h1>
                 <p><a className='link' href='tel:+351938349977'>+351 938 349 977</a></p>
                 <p><a className='link' href='mailto:luisclaudioc@hotmail.com'>luisclaudioc@hotmail.com</a></p>
-                <p>rua cidade de cardiff 17 r/c</p>
-                <p style={{marginBottom: "20px"}}>1170-094, lisbon, portugal</p>
+                <p>Türkenstraße 26</p>
+                <p style={{marginBottom: "20px"}}>, 80333 munich, germany</p>
                 <ContactForm />
                 <h1>my_social_media:</h1>
                 <p>Connect with me on <a href='google.com' className="link" target="_blank">LinkedIn</a>, <a href='google.com' className="link" target="_blank">GitHub</a> or <a href='google.com' className="link" target="_blank">Instagram</a></p>
